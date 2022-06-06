7bb682dada0b4be553936b4f3542b1fc7edb45b0
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2022-06-06 14:52:15 +0430

    list comprehensions

---
+++ list_comprehensions.py
@@ -0,0 +1,14 @@
+
+# lst = [x for x in 'hello']
+# print(lst)
+#
+# lst1 = [x**2 for x in range(0, 11)]
+# print(lst1)
+
+# Convert Celsius to Fahrenheit
+# celesius = [0, 10, 20.1, 34.5]
+# fahrenheit = [((x*9/5) + 32) for x in celesius]
+# print(fahrenheit)
+
+lst = [x * 2 for x in [y for y in range(5)]]
+print(lst)
