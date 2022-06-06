4f9c1daca98c90d062a49eae84e8c1cf8b62bd64
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2022-06-06 12:02:06 +0430

    added new code set and bool

--- Sets & Bools.py
+++ Sets & Bools.py
@@ -1,1 +1,21 @@
+
+# x = {1, 2, 3, 4}
 x = set()
+
+x.add(1)
+x.add(2)
+x.add(3)
+x.add(1)
+x.add(4)
+x.add(4)
+print(x)
+
+lst = [1, 2, 3, 4, 5, 6, 1, 2, 3]
+print(lst)
+print(list(set(lst)))
+
+# bb: bool = False
+bb = False
+No = None
+print(bb)
+print(None)
