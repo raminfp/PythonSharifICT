11fe607a6c478cd8b71ab6e24c642a0557d6d1d2
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2022-06-11 12:19:17 +0430

    number and string code

---
+++ numbers.py
@@ -0,0 +1,22 @@
+
+# print(hex(42))
+# print(hex(1))
+# print(hex(16))
+# print(hex(10))
+# print(hex(11))
+# print(hex(15))
+# print(hex(12))
+# print(hex(70))
+# print(hex(63))
+# print(hex(120))
+
+## Binary
+print(bin(1))
+print(bin(10))
+print(bin(1000))
+
+## Pow
+print(pow(2, 4))
+print(pow(9, 2))
+print(pow(10, 10))
+
---
+++ strings.py
@@ -0,0 +1,15 @@
+s = "hell  oworld  "
+f = s.center(20, 'z')
+print(f)
+print(len(f))
+# print(s.isalnum())
+# print(s.isalpha())
+# for a in s:
+#     print(a.isspace())
+
+print((' ' in s))
+print(s.isdigit())
+
+print(s.strip())
+print(s.partition("l"))
+print(s.split("e"))
